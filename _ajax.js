0419f4b4190337be32c34b59c6db760b0710c9e4
ksaanen <ksaanen@independer.nl>
2018-12-12 13:14:28 +0100

    attach to window

--- _ajax.js
+++ _ajax.js
@@ -1,32 +1,34 @@
-let _ajax = {}
+(function(window){
 
-function requestHandler(req, func) {
-  if (req.readyState === XMLHttpRequest.DONE && req.status === 200) {
-    return func;
+  window._ajax = {}
+
+  function requestHandler(req, func) {
+    if (req.readyState === XMLHttpRequest.DONE && req.status === 200) {
+      return func;
+    }
   }
-}
-
-_ajax.$get = function({url, success}) {
-  let request = new XMLHttpRequest();
-
-  request.onreadystatechange = requestHandler(request, function(){
-    success(request.responseText)
-  });
   
-  request.open('GET', url);
-  request.send();
-}
-
-_ajax.$post = function({url, data, success}) {
-  let request = new XMLHttpRequest();
-
-  request.onreadystatechange = requestHandler(request, function(){
-    success(request.responseText)
-  });
-
-  request.open('POST', url);
-  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  request.send(data);
-}
-
-
+  window._ajax.$get = function({url, success}) {
+    let request = new XMLHttpRequest();
+  
+    request.onreadystatechange = requestHandler(request, function(){
+      success(request.responseText)
+    });
+    
+    request.open('GET', url);
+    request.send();
+  }
+  
+  window._ajax.$post = function({url, data, success}) {
+    let request = new XMLHttpRequest();
+  
+    request.onreadystatechange = requestHandler(request, function(){
+      success(request.responseText)
+    });
+  
+    request.open('POST', url);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    request.send(data);
+  }
+  
+})(window);
